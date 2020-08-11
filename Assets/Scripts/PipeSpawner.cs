272bd63514a5a2872f63e2dec9c039f0fe213367
Moerte86 <nunoliveira86@gmail.com>
2020-08-11 22:38:27 +0100

    First and last commit

---
+++ Assets/Scripts/PipeSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawner : MonoBehaviour
+{
+    public float maxTimer = 1.5f;
+    float timer = 0;
+    public GameObject column;
+    public float minHeight, maxHeight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject newColumn = Instantiate(column);
+        newColumn.transform.position = (Vector2)transform.position + Vector2.up * Random.Range(minHeight, maxHeight);
+        Destroy(newColumn, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer > maxTimer)
+        {
+            GameObject newColumn = Instantiate(column);
+            newColumn.transform.position = (Vector2)transform.position + Vector2.up * Random.Range(minHeight, maxHeight);
+            Destroy(newColumn, 5);
+            timer = 0;
+        }
+        timer += Time.deltaTime;
+        
+    }
+}
